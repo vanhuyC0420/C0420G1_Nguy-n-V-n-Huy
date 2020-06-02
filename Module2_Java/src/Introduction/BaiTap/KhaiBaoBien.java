7b2de461da552b5c25ee20665327b9b9f1d627ad
vanhuyC0420 <vanhuy0108@gmail.com>
2020-06-02 13:55:33 +0700

    up lên

---
+++ Module2_Java/src/Introduction/BaiTap/KhaiBaoBien.java
@@ -0,0 +1,18 @@
+package Introduction.BaiTap;
+
+public class KhaiBaoBien {
+    public static void main(String[] args) {
+        int a = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = false;
+        char c = 'a';
+        String s = "Quảng Bình";
+        System.out.println("a = " + a);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.print("s = " + s);
+    }
+}
